a1738d5d31804af1224bf07b6722283eb2e7999f
rhasson <rhasson@gmail.com>
2013-02-26 00:07:59 -0500

    initial integration of m-wire

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var db = require('./lib/db.js');
+
+exports.routes = {
+	v1 : {
+		get: function(req, res, next) {
+
+		},
+		set: function(req, res, next) {
+
+		},
+		del: function(req, res, next) {
+
+		},
+		find: function(req, res, next) {
+
+		}
+	}
+}
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var restify = request('restify'),
+var restify = require('restify'),
     server = restify.createServer(),
-	io = request('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	ecstatic = require('ecstatic');
+
+var routes = require('./routes').routes;
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.dateParser());
@@ -8,14 +11,21 @@ server.use(restify.queryParser());
 server.use(restify.gzipResponse());
 server.use(restify.bodyParser());
 
-server.get('/', function(req, res, next) {
+server.get('/:id', routes.v1.get);
+server.get('/:id/:key', routes.v1.get);
 
-});
+server.post('/', routes.v1.set);
+server.put('/', routes.v1.set);
+
+server.del('/:id', routes.v1.del);
+server.del('/:id/:key', routes.v1.del);
+
+server.get(/\/?.*/, ecstatic({ root: __dirname + '/public' }));
 
 io.sockets.on('connection', function(socket) {
-
+	socket.emit('news', {hello: 'world'});
 });
 
 server.listen(80, function() {
-	connsole.log('Server started on port ', server.port());
+	console.log('Server started ', new Date());
 })
